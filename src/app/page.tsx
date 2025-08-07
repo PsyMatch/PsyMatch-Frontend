eaf2e46318fb9d3ad652eddffce9c0905b067d11
morenamartin <martinmorena269@gmail.com>
2025-08-07 14:35:48 -0300

    Formulario Registro Professional, correccion de navBar y Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,9 @@ import Footer from "@/components/Footer";
 import Information from "@/components/Home/Information/Information";
 import Match from "@/components/Home/Match/Match";
 import Profesionales from "@/components/Home/profesionales/Profesionales";
-
-import Information from '@/components/Home/Information/Information';
-import Match from '@/components/Home/Match/Match';
-import Profesionales from '@/components/Home/profesionales/Profesionales';
 import Start from '@/components/Home/Start/Start';
 
 export default function Home() {
-  return (
-    <>
-      <Match />
-      <Information />
-      <Profesionales />
-    </>
-  );
     return (
         <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
             <Match />
