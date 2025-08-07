4800867d2fd78ded06ba3992d7534ac64cf8537f
Ludmila <lud.ravelli@gmail.com>
2025-08-06 23:39:26 -0300

    feat: add CardInformation component and update Information section on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Information from '@/components/Home/Information/Information';
 import Match from '@/components/Home/Match/Match';
 
 export default function Home() {
     return (
         <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
             <Match />
+            <Information />
         </div>
     );
 }
