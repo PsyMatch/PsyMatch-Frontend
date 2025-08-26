2870df2bbc1190152de963f99820b386b0ae2e4a
morenamartin <martinmorena269@gmail.com>
2025-08-26 11:13:32 -0300

    error en reseñas del dashboar del profesional

--- src/components/dashboard-profesional/Reseñas.tsx
+++ src/components/dashboard-profesional/Reseñas.tsx
@@ -21,34 +21,40 @@ const Reseñas = () => {
             .catch(console.error);
     }, []);
 
+    const [loader, setLoader] = useState(true)
 
     useEffect(() => {
-        const token = Cookies.get('auth_token');
-        if (!token) return;
-
-        const id = Cookies.get("id")
-
-        fetch(`${envs.next_public_api_url}/reviews/${id}`, {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
-            .then((res) => res.json())
-            .then((response) => {
-                setReseñas(response.reviews.reviews);
+        try{
+            const token = Cookies.get('auth_token');
+            if (!token) return;
+    
+            const id = Cookies.get("id")
+    
+            fetch(`${envs.next_public_api_url}/reviews/${id}`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
             })
-            .catch(console.error);
+                .then((res) => res.json())
+                .then((response) => {
+                    setReseñas(response.reviews.reviews);
+                    setLoader(false)
+                })
+                .catch(console.error);
+        }catch(err){
+            console.error(err)
+            setLoader(false)
+        }
     }, [idUser]);
 
     return (
         <div className="flex flex-col gap-3 px-8 py-8 h-fit">
             <div>
                 <h1 className="text-xl font-semibold text-black">Reseñas sobre vos</h1>
-                <span className="text-black">Análisis de desempeño y temas de tus reseñas</span>
+                {loader === true ? <span className="text-black">Cargando reseñas...</span> : <span className="text-black">Análisis de desempeño y temas de tus reseñas</span>}
             </div>
-            <div>{!reseñas && 'Cargando reseñas...'}</div>
-            <div>
+            <div> 
                 {reseñas?.map((res, index) => (
                     <div key={index} className="items-center w-full px-5 py-3 my-4 bg-gray-200 border-2 border-gray-300 rounded-lg ">
                         <div className="flex flex-row justify-between mb-3">
